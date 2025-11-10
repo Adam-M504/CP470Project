9e811c3bea5b12ab66a16595c83579856e60fa23
Michael D <md433172@gmail.com>
2025-11-09 19:12:36 -0500

    second pass - rudementary database connection, debugging for home page to add games to db

--- app/src/main/java/com/example/unnamedapp/HomePage.java
+++ app/src/main/java/com/example/unnamedapp/HomePage.java
@@ -1,6 +1,13 @@
 package com.example.unnamedapp;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -18,6 +25,9 @@ public class HomePage extends AppCompatActivity {
     RecyclerView dateRecyclerView, gameRecyclerView;
     DateAdapter dateAdapter;
     GameAdapter gameAdapter;
+    DatabaseHelper myhelper;
+    SQLiteDatabase db;
+    ImageButton Trophy_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +46,64 @@ public class HomePage extends AppCompatActivity {
         dateRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         dateRecyclerView.setAdapter(dateAdapter);
 
+        Trophy_button = findViewById(R.id.navStats);
+
+        myhelper = new DatabaseHelper(this);
+        db = myhelper.getWritableDatabase();
+
+        Trophy_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+                values.put(DatabaseHelper.KEY_G_DATE, "November 9th");
+                values.put(DatabaseHelper.KEY_G_LOCATION, "Field 1");
+                values.put(DatabaseHelper.KEY_G_TIME, "6PM");
+                values.put(DatabaseHelper.KEY_G_T1, "1");
+                values.put(DatabaseHelper.KEY_G_T2, "2");
+                values.put(DatabaseHelper.KEY_G_T1_SCORE, "0");
+                values.put(DatabaseHelper.KEY_G_T2_SCORE, "0");
+                values.put(DatabaseHelper.KEY_G_REF_ID, "1");
+                db.insert(DatabaseHelper.TABLE_NAME_GAME, null, values);
+                Log.i("HomePage","Database Updated, Game Added");
+            }
+        });
+
         // game list
         List<Game> games = new ArrayList<>();
-        games.add(new Game("Game1", "3PM", "Field 1"));
-        games.add(new Game("Game2", "5PM", "Field 2"));
-        games.add(new Game("Game3", "6PM", "Field 3"));
+
+        String get_game_values = "SELECT "+DatabaseHelper.KEY_G_ID +", "+DatabaseHelper.KEY_G_TIME+", "
+                +DatabaseHelper.KEY_G_LOCATION+" FROM " + DatabaseHelper.TABLE_NAME_GAME;
+
+        Cursor cursor = db.rawQuery(get_game_values, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()){
+            Log.i("ChatWindow", "Cursor's column count ="+cursor.getColumnCount());
+            String games_id = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.KEY_G_ID));
+            String games_time = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.KEY_G_TIME));
+            String games_location = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.KEY_G_LOCATION));
+            games.add(new Game(games_id, games_time,games_location));
+
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+
+//        games.add(new Game("Game1", "3PM", "Field 1"));
+//        games.add(new Game("Game2", "5PM", "Field 2"));
+//        games.add(new Game("Game3", "6PM", "Field 3"));
 
         gameAdapter = new GameAdapter(games);
         gameRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         gameRecyclerView.setAdapter(gameAdapter);
+    }
+
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("HomePage","onDestroy, Closing Database");
+        db.close();
     }
 }
